8484b28eba518adab97e8413992667b153bd5f0f
Eric Energy <ericnrgnash@gmail.com>
2019-12-17 09:48:50 -0600

    creating fille structure

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name, devoured) values ("Burger One", false);
+INSERT INTO burgers (burger_name, devoured) values ("Burger Two", false);
+INSERT INTO burgers (burger_name, devoured) values ("Burger Three", false);
