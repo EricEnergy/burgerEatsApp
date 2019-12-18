84c0f998e8b649749638d2b054f68cd3234da48b
Eric Energy <ericnrgnash@gmail.com>
2019-12-17 23:05:53 -0600

    <test>

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,31 @@
 var connection = require("../config/connection.js");
 
+var orm = {
 
+ selectWhere: function(tableInput, cb) {
+  var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
 
-selectAll()
-insertOne()
-updateOne()
+  insertOne: function(val1, cb) {
+    var queryString = "INSERT INTO `burgers`(burger_name) VALUES (?)";
+    connection.query(queryString, [val1], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
 
+  updateOne: function (idNum, cb) {
+    var queryString = "UPDATE burgers SET devoured = true WHERE `id` = ?;";
+    connection.query(queryString, [idNum], function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+};
 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(250) NOT NULL,
-    devoured BOOLEAN DEFAULT false
+    devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 
 );
